2b6dc498c8b75e587ae2781788bb8901ef7692b8
David Mazza <github@dmzza.com>
2015-11-06 23:49:17 -0800

    Count windows

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -5,6 +5,20 @@ class Listing < ActiveRecord::Base
     rents.last.price
   end
 
+  def windows
+    count = 0
+    if left_window
+      count += 1
+    end
+    if middle_window
+      count += 1
+    end
+    if right_window
+      count += 1
+    end
+    count
+  end
+
   def value_score
     latest_price.to_f / sqft.to_f
   end
